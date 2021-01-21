21ef1168064df5edb99180f85cea73dcd74a2f6b
Park Yong Ha <yonghapk@gmail.com>
2021-01-22 00:05:45 +0900

    MODIFY list

--- pages/todo/index.vue
+++ pages/todo/index.vue
@@ -20,6 +20,40 @@
 						</div>
 					</div>
 				</li>
+				<li>
+					<div class="list__category">카테고리</div>
+					<div class="list__info">
+						<div class="list__title">
+							소스코드 정리
+						</div>
+						<div class="list__desc">
+							Vue + Nuxt 프로젝트
+						</div>
+					</div>
+					<div class="list__meta">
+						<div class="list__dday">D-3</div>
+						<div class="list__location">
+							11-20 11:11 E마트
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="list__category">카테고리</div>
+					<div class="list__info">
+						<div class="list__title">
+							소스코드 정리
+						</div>
+						<div class="list__desc">
+							Vue + Nuxt 프로젝트
+						</div>
+					</div>
+					<div class="list__meta">
+						<div class="list__dday">D-3</div>
+						<div class="list__location">
+							11-20 11:11 E마트
+						</div>
+					</div>
+				</li>
 			</ul>
 		</div>
 	</div>
@@ -32,10 +66,33 @@ export default {
 </script>
 
 <style lang="scss">
+.todo-list {
+	padding:30px 0;
+}
 .list {
 	li {
+		display:flex;
+		align-items:center;
+		padding:20px 0;
+		border-bottom:1px solid #ccc;
+	}
+	&__category {
 		position:relative;
-		padding-left:100px;
+		flex-basis:120px;
+		flex-grow:0;
+	}
+	&__info {
+		flex-grow:1;
+	}
+	&__title {
+		font-size:36px
+	}
+	&__desc {
+		font-size:18px;
+		color:#666
+	}
+	&__meta {
+		flex-grow:1;
 	}
 }
 </style>
