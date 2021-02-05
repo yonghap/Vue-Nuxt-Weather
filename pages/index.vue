383907d84aafbaa4bc47a7d2bc09881e92acabb7
Park Yong Ha <yonghapk@gmail.com>
2021-02-05 21:23:40 +0900

    ADD api

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,24 @@
 <template>
-	<div id="intro" class="intro">
-		<div class="intro__wrap">
-			<div class="intro__inner">
-				<h1 class="logo">
-					<strong>Todo</strong> Lists
-				</h1>
-				<div class="intro__btn">
-					<NuxtLink to="/todo" class="btn-intro">
-						+
-					</NuxtLink>
-				</div>
-			</div>
-		</div>
+	<div id="main" class="main">
+
 	</div>
 </template>
 
+<script>
+export default {
+	created() {
+		this.$axios.get('https://api.openweathermap.org/data/2.5/weather', {
+			params: {
+				q : 'seoul',
+				appid : '754f7bf1ddc3ba9c85002d9fb4143682'
+			},
+		}).then(res => {
+			console.log(res.data)
+		});
+	}
+}
+</script>
+
 <style lang="scss">
-.intro {
-	position:absolute;
-	top:0;
-	left:0;
-	width:100%;
-	height:100%;
-	text-align:center;
-	&__wrap {
-		display:flex;
-		height:100%;
-		justify-content:center;
-		align-items:center;
-	}
-	&__btn {
-		margin-top:25px
-	}
-}
-.logo {
-	font-weight:normal;
-	strong {
-		font-weight:bold;
-	}
-}
-.btn-intro {
-	display:inline-block;
-	width:60px;
-	height:60px;
-	border:1px solid #ccc;
-}
+
 </style>
