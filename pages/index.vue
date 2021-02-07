fdfde4c13f0e6141972172a9d442efe7b9572954
Park Yong Ha <yonghapk@gmail.com>
2021-02-07 23:02:38 +0900

    ADD temp

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
 	<div id="main" class="main">
-
+		<div class="info box" id="info">
+			<div class="info__status" v-if="weatherData">
+				<img src="~@/assets/images/sunny.png">
+				<div class="info__temp">
+					<strong>{{ parseInt(weatherData.main.temp - 273.15) }}</strong>°
+				</div>
+				<div class="info__number">
+					<strong>{{ parseInt(weatherData.main.temp_max - 273.15) }}</strong>°
+					/
+					<strong>{{ parseInt(weatherData.main.temp_min - 273.15) }}</strong>°
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			weatherData : null
+		}
+	},
 	created() {
 		this.$axios.get('https://api.openweathermap.org/data/2.5/weather', {
 			params: {
@@ -14,6 +31,7 @@ export default {
 			},
 		}).then(res => {
 			console.log(res.data)
+			this.weatherData = res.data;
 		});
 	}
 }
