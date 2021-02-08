72b16c8c690df436227f46c0f7747c8f6f2801bf
Park Yong Ha <yonghapk@gmail.com>
2021-02-09 02:21:25 +0900

    ADD multiple axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<div id="main" class="main">
 		<div class="info box" id="info">
-			<div class="info__status" v-if="weatherData">
+			<div class="info__status" v-if="weatherData.currentWeather">
 				<img src="~@/assets/images/sunny.png">
 				<div class="info__temp">
-					<strong>{{ parseInt(weatherData.main.temp - 273.15) }}</strong>°
+					<strong>{{ parseInt(weatherData.currentWeather.main.temp - 273.15) }}</strong>°
 				</div>
 				<div class="info__number">
-					<strong>{{ parseInt(weatherData.main.temp_max - 273.15) }}</strong>°
+					<strong>{{ parseInt(weatherData.currentWeather.main.temp_max - 273.15) }}</strong>°
 					/
-					<strong>{{ parseInt(weatherData.main.temp_min - 273.15) }}</strong>°
+					<strong>{{ parseInt(weatherData.currentWeather.main.temp_min - 273.15) }}</strong>°
 				</div>
 			</div>
 		</div>
@@ -20,19 +20,31 @@
 export default {
 	data() {
 		return {
-			weatherData : null
+			weatherData : {
+				currentWeather : '',
+				forecastWeather : ''
+			}
 		}
 	},
 	created() {
-		this.$axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		let $vm = this,
+			currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather',
+			forecastWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+		let prm = {
 			params: {
 				q : 'seoul',
 				appid : '754f7bf1ddc3ba9c85002d9fb4143682'
-			},
-		}).then(res => {
-			console.log(res.data)
-			this.weatherData = res.data;
+			}
+		}
+		const currentWeatherData = this.$axios.get(currentWeatherUrl,prm),
+			forecastWeatherData = this.$axios.get(forecastWeatherUrl,prm);
+
+		Promise.all([currentWeatherData, forecastWeatherData]).then(function(values) {
+			$vm.weatherData.currentWeather = values[0].data;
+			$vm.weatherData.forecastWeather = values[1].data;
+			console.log($vm.weatherData);
 		});
+
 	}
 }
 </script>
