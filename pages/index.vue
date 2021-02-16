4a6b9b35e1ffbaf8f06d435d51c5908afe129d22
Park Yong Ha <yonghapk@gmail.com>
2021-02-17 03:46:36 +0900

    ADD today hourly forecast

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-	<div id="main" class="main" v-if="weatherData.currentWeather">
+	<div id="main" class="main" v-if="weatherData">
 		<section class="box">
 			<article class="current">
 				<div class="row">
 					<div class="col">
 						<div class="current__temp">
-							{{ parseInt(weatherData.currentWeather.main.temp - 273.15) }}°
+							{{ parseInt(weatherData.current.temp - 273.15) }}°
 						</div>
 						<div class="current__main">
-							{{ weatherData.currentWeather.weather[0].main }}
+							{{ weatherData.current.weather[0].main }}
 						</div>
 					</div>
 					<div class="col">
 						<div class="current__icon">
-							<img src="~@/assets/images/sunny.png">
+							<img src="~@/assets/images/partly-cloudy.png">
 						</div>
 					</div>
 				</div>
@@ -32,23 +32,45 @@
 			<article class="etc">
 				<div class="col">
 					<strong>
-						{{ weatherData.currentWeather.main.humidity }}%
+						{{ weatherData.current.humidity }}%
 					</strong>
 					Humidity
 				</div>
 				<div class="col">
 					<strong>
-						{{ parseInt(weatherData.currentWeather.main.feels_like - 273.15) }}°
+						{{ parseInt(weatherData.current.feels_like - 273.15) }}°
 					</strong>
 					Feels Like
 				</div>
 				<div class="col">
 					<strong>
-						{{ weatherData.currentWeather.wind.speed }}
+						{{ weatherData.current.wind_speed }}
 					</strong>
 					Wind
 				</div>
 			</article>
+		</section>
+		<section class="box">
+			<h2 class="title">
+				Today
+			</h2>
+			<ul class="list">
+				<li v-for="n in 4">
+					<div class="list__icon">
+						<img src="~@/assets/images/sunny.png" v-if="n == 1">
+						<img src="~@/assets/images/partly-cloudy.png" v-if="n == 2">
+						<img src="~@/assets/images/sleet.png" v-if="n == 3">
+						<img src="~@/assets/images/showers.png" v-if="n == 4">
+					</div>
+					<div class="list__temp">
+						{{ parseInt(weatherData.hourly[n * 3].temp - 237.15) }}°
+					</div>
+					<div class="list__time">
+						{{ translateTimeStamp(weatherData.hourly[n * 3].dt) }}
+
+					</div>
+				</li>
+			</ul>
 		</section>
 	</div>
 </template>
@@ -57,38 +79,31 @@
 export default {
 	data() {
 		return {
-			weatherData : {
-				currentWeather : '',
-				forecastWeather : ''
-			}
+			weatherData : ''
 		}
 	},
 	methods : {
 		translateTimeStamp(dt) {
 			let newDate  = new Date(dt * 1000);
-			return newDate.getFullYear() + "-" + (newDate.getMonth()+1) + "-" + newDate.getDate() +
-				" " + newDate.getHours() + ":" + newDate.getMinutes();
+			return newDate.getHours() + ":" + newDate.getMinutes() + '0';
+			// return newDate.getFullYear() + "-" + (newDate.getMonth()+1) + "-" + newDate.getDate() +
+			// 	" " + newDate.getHours() + ":" + newDate.getMinutes();
+		},
+		async fetchWeather() {
+			const prm = {
+				params: {
+					lat : '37.5683',
+					lon : '126.9778',
+					exclude : '',
+					appid : '754f7bf1ddc3ba9c85002d9fb4143682'
+				}
+			}
+			const weather = await this.$axios.$get('https://api.openweathermap.org/data/2.5/onecall', prm);
+			this.weatherData = weather;
 		}
 	},
 	created() {
-		let $vm = this,
-			currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather',
-			forecastWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-		let prm = {
-			params: {
-				q : 'seoul',
-				appid : '754f7bf1ddc3ba9c85002d9fb4143682'
-			}
-		}
-		const currentWeatherData = this.$axios.get(currentWeatherUrl,prm),
-			forecastWeatherData = this.$axios.get(forecastWeatherUrl,prm);
-
-		Promise.all([currentWeatherData, forecastWeatherData]).then(function(values) {
-			$vm.weatherData.currentWeather = values[0].data;
-			$vm.weatherData.forecastWeather = values[1].data;
-			console.log($vm.weatherData);
-		});
-
+		this.fetchWeather();
 	}
 }
 </script>
